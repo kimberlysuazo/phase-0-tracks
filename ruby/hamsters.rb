1c0f6816472ea7aa121752164abf8b52cf849ef3
kimberlysuazo <chotoberli@hotmail.com>
2016-07-11 15:49:26 -0400

    convert strings to appropriate data type

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -2,16 +2,23 @@ puts "What is the hamster's name?"
 hamster_name = gets.chomp
 
 puts "What is your hamster's volume level (from 1 to 10)?"
-volume_level = gets.chomp
+volume_level = gets.chomp.to_i
 
 puts "What is your hamster's fur color?"
 fur_color = gets.chomp
 
-puts "Is your hamster a good candidate for adoption?"
+puts "Is your hamster a good candidate for adoption (y/n)?"
 adoption_cadidate = gets.chomp
 
+if adoption_cadidate == "y"
+  adoption_cadidate = true
+else 
+  adoption_cadidate = false
+end   
+
+
 puts "What is your hamster's estimated age?"
-age = gets.chomp
+age = gets.chomp.to_f
 
 if age == ""
   age = nil 
