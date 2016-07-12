0558f6f549a966f2fc1d5ec497e7954c0380043e
kimberlysuazo <chotoberli@hotmail.com>
2016-07-12 15:18:42 -0400

    Add survey questions and if/else conditional for release 1 and 2

---
+++ ruby/vampires.rb
@@ -0,0 +1,66 @@
+
+current_year = 2016
+
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age_given = gets.chomp
+
+puts "What year were you born?"
+birth_year = gets.chomp
+
+actual_age = current_year - birth_year
+if actual_age == age_given
+   correct_age = true 
+else 
+  correct_age = false
+end   
+
+puts "Our cafeteria serves garlic bread. Should we order some for you (y/n)?"
+garlic_bread = gets.chomp
+if garlic_bread == "y"
+  garlic_bread = true
+else 
+  garlic_bread = false
+end   
+
+puts "Would you like to enroll in the company's health insurance (y/n)?"
+health_insurance = gets.chomp
+
+if health_insurance == "y"
+  health_insurance = true
+else 
+  health_insurance = false
+end     
+
+
+
+case
+when (name) && ((correct_age) && (garlic_bread || health_insurance))
+  puts "Probably not a vampire."
+
+when (name) && ((!garlic_bread || !health_insurance) && (!correct_age))
+  puts "Probably a vampire."
+  
+when (name) && ((!correct_age) && (!garlic_bread && !health_insurance))
+  puts "Almost certainly a vampire."
+
+when !name
+  puts "Definitely a vampire."
+  
+else 
+  puts "Results inconclusive" 
+end  
+  
+  
+
+ 
+
+
+
+
+
+
+
+
