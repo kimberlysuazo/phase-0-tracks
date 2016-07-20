193b584aca6c9d4bbc6f3c63c28bc037c991d48d
kimberlysuazo <chotoberli@hotmail.com>
2016-07-20 19:48:52 -0400

    Initial commit

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,16 @@
+=begin
+Swapping the first and last name.
+Changing all of the vowels (a, e, i, o, or u) to the next vowel in 'aeiou', 
+and all of the consonants (everything else besides the vowels) to the next consonant in the alphabet. 
+So 'a' would become 'e', 'u' would become 'a', and 'd' would become 'f'.
+=end
+
+#PSEUDOCODE 
+=begin
+-  
+
+  
+=end
+
+full = "Kimberly Suazo"
+full.split("")
