ecce4b8cf1bb168a086e38bf10801846eb3959d3
kimberlysuazo <chotoberli@hotmail.com>
2016-07-20 14:26:50 -0400

    Add content for mall hash. Code first floor hash.

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,129 @@
+
+
+
+mall = {
+  floor_1 = {
+      'Victorias Secret': [
+        'Bras',
+        'Panties',
+        'Pajamas',
+        'Perfumes',
+        'Lotions'
+        ],
+      'Best Buy': [
+        'TVs',
+        'Computers',
+        'Cellphones'
+        ],
+      'Old Navy': [ 
+        'Pants',
+        'Shirts',
+        'Skirts',
+        'Dresses',
+        'Flip-flops'
+        ]
+  }   
+     
+=begin
+Floor 2: HASH
+  
+  Food court HASH
+    Burger King =HASH
+        Burgers: 
+            [
+            Whopper
+            Whopper Jr 
+            ]
+        Chicken Sandwiches:
+            [
+            Tendergrill chicken sandwich 
+            Original Chicken Sandwich 
+            Club Chicken Sandwich
+            ]
+        Side Items: 
+          [
+          Onion rings
+          French Fries
+          Garden Salad
+          ] 
+
+    Jamba Juice (key)
+      [
+      Smoothies
+      Wheatgrass
+      Fruit
+      ]
+    Starbucks =HASH
+        Furniture: (Key)
+          [
+          Chairs
+          Tables
+          Stools
+          ]
+        Food:
+          [Coffee
+          Milk
+          Tea
+          Pastries
+          Sandwiches]
+    Chick-Fil-A =HASH
+        Classics: (Key) 
+          [
+          Chick-fil-A Nuggets
+          Chick-fil-A Chick-n-Strips
+          Chick-fil-A Chicken Sandwich
+          ]
+        Side Items:
+          [
+          Chick-fil-A Waffles Fries
+          Chicken Soup
+          Fruit Cup 
+          Side Salad
+          ]
+        Desserts: 
+          Chocolate Cookie 
+          Icedream
+          Milkshakes: 
+            [
+            Cookies and Cream
+            Peach
+            Strawberry
+            Vanilla
+            ] 
+        
+
+  Restrooms HASH
+    Male: (Key)
+      [Toilet booths 
+      Urinals
+      Sinks
+      Mirrors]
+    Female: (Key)
+      [Toilet booths 
+      Sinks
+      Mirrors]
+
+
+Floor 3: HASH
+  Movie theather (Key)=>
+    [ 
+    Ticket Booth 
+    Concession Stand
+    Theather Room 1 
+    Theater Room 2
+    ]
+
+Floor -1 : HASH
+  parking lot (Key)=> cars 
+
+
+}
+=end 
+
+
+
+=begin
+Once you've built it, print a few individual pieces of deeply nested data from the structure, 
+showing that you know how to use multiple indexes or hash keys (or both) to access nested items. 
+Try to demonstrate a few different types of access.
+=end 
