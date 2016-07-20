c35ea6915af7eecabd01b2fd2c408ebc8cca82bf
kimberlysuazo <chotoberli@hotmail.com>
2016-07-19 21:11:12 -0400

    Create method to ask questions

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -15,26 +15,27 @@ your program should ask for a new value and update the :decor_theme key.
 (Hint: Strings have methods that will turn them into symbols, which would be quite handy here.) 
 You can assume the user will correctly input a key that exists in your hash -- no need to handle user errors.
 Print the latest version of the hash, and exit the program.
+pseudocode
 
 =end 
 
-puts "What is your full name?"
-full_name = gets.chomp.capitalize
+puts "Question 1: What is your full name?"
+full_name = gets.chomp
 
-puts "How old are you?"
+puts "Question 2: How old are you?"
 age_answer = gets.chomp.to_i
 
-puts "How many children do you have?"
+puts "Question 3: How many children do you have?"
 children_answer = gets.chomp.to_i
 
-puts "Is your living space 'open-concept' (i.e., the living room, dining and kitchen areas are not enclosed by walls)? Please answer \"yes\" or \"no\"."
+puts "Question 4: Is your living space 'open-concept' (i.e., the living room, dining and kitchen areas are not enclosed by walls)? Please answer \"yes\" or \"no\"."
 apt_style = gets.chomp.downcase
 has_open_concept = false
 if apt_style == "yes"
   has_open_concept = true
 end   
 
-puts "What is your preferred decor theme?"
+puts "Question 5: What is your preferred decor theme?"
 theme_answer = gets.chomp
 
 client_form = {
@@ -49,8 +50,52 @@ p client_form
 
 puts "Would you like to update any of the answers? Please answer \"yes\" or \"no\"."
 update = gets.chomp
-if 
+
+if update == "yes"
+  puts "Please enter the number of the question you'd like to update (1 - 5)."
+  question_number = gets.chomp.to_i
+  puts "Please enter the corrected answer."
+  corrected_answer = gets.chomp
+end
+
+question_update = {:name => 1, :age => 2, :children_amount => 3, :open_concept => 4, :decor_theme => 5}
 
 
+client_form[question_update.key(question_number)] = corrected_answer
+
+p client_form
+
+
+ask_question(question_number)
+
+def ask_question(n)
+case 
+  when n == 1
+    puts "Question 1: What is your full name?"
+    $full_name = gets.chomp
+  when n == 2
+    puts "Question 2: How old are you?"
+    $age_answer = gets.chomp.to_i
+  when n == 3
+    puts "Question 3: How many children do you have?"
+    $children_answer = gets.chomp.to_i
+  when n == 4
+    puts "Question 4: Is your living space 'open-concept' (i.e., the living room, dining and kitchen areas are not enclosed by walls)? Please answer \"yes\" or \"no\"."
+    apt_style = gets.chomp.downcase
+    $has_open_concept = false
+      if apt_style == "yes"
+        $has_open_concept = true
+      end   
+  else n == 5
+    puts "Question 5: What is your preferred decor theme?"
+    $theme_answer = gets.chomp     
+  end 
+end 
+
+ask_question(1)
+ask_question(2)
+ask_question(3)
+ask_question(4)
+ask_question(5)
 
 
