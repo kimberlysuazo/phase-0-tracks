53625a324e96c438ace88a255cf4ed48c4ae3abd
kimberlysuazo <chotoberli@hotmail.com>
2016-07-21 02:27:33 -0400

    Add pseudocode. Complete assignment

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,5 +1,34 @@
+#PSEUDOCODE
+=begin
+- Create a method that cycles through the letters of the word entered checking if the letters are vowels or consonants.
+    -Create the following variables:
+      -vowels (listing vowels)
+      -consonants (listing consonants)
+      -new first (empty array) 
+    -If the letters are vowels, 
+        -match that letter to the index of variable listing vowels, and add 1. 
+        -store that index number in new variable x 
+        -Create new variable z which uses the [x] to call up the letter. 
+        -push values into new_first array using the .insert method. 
+    - If letter are consonants, do same thing but going through consonants variable and using different variable names. 
+    -create new_name which stores the new_first array joined into one string. Capitalize. 
+ -end method
+ -Make empty hash to store future names and corresponding aliases. 
+ -begin loop that goes on until user types "quit".
+  -In loop, ask the user to type in a name. 
+    -Split up that name into an array of two values. 
+      -Make first_name and last_name variables from those two values. 
+    -Use last_name and first_name variables to call the alias_makes method. 
+    -Store new alieas name in new variable: new_full_name, mixing up the first and last name order.  
+    -Push new name to names_and_aliases hash. 
+    -Print the new alias. 
+  -end loop
+  -print the names_and_aliases hash contents      
+=end 
+
+#DEFINE METHOD
 def alias_maker (name)
-  vowels= "aeioua"
+    vowels= "aeioua"
     consonants = "bcdfghjklmnpqrstvwxyzb"
     new_first = []
     n= 0
@@ -18,10 +47,14 @@ def alias_maker (name)
     new_name= new_first.join("").capitalize
 end   
 
+#DRIVER CODE
+
 names_and_aliases = {}
 loop do 
   puts "Please enter the full name (first and last) to change into an alias. If done type \"quit\"."
-  name_entered = gets.chomp
+  name_entered = gets.chomp 
+  #used separate variable for so that when printing the names_and_aliases hash, 
+  #the name entered is capitalized (or listed as entered, NOT #downcased).
   full_name= name_entered.downcase
   break if full_name == "quit" 
 
