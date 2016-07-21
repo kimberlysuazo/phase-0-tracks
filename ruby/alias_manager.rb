00cd0bf5c756eab2cea00fd3613b5c462b67b474
kimberlysuazo <chotoberli@hotmail.com>
2016-07-20 23:12:00 -0400

    Complete entire method

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -12,50 +12,54 @@ So 'a' would become 'e', 'u' would become 'a', and 'd' would become 'f'.
 Remember .upcase at the end. right before creating new array. 
   
 =end
+def alias_maker 
+puts "Please type in the full name to change into an alias."
+full_name= gets.chomp.downcase
+   vowels= "aeioua"
+  consonants = "bcdfghjklmnpqrstvwxyzb"
 
-#Puts "Please type in the full name to change into an alias."
-#full_name= gets.chomp.downcase
-
-#alias_maker(full_name)
-def alias_maker
-  vowels= "aeiou"
-  consonants = "bcdfghjklmnpqrstvwxyz"
-
-  full_name = "kimberly suazo"
+  #full_name = "kimberly suazo"
+  
   full = full_name.split(" ")
  
   original_first_name = full[0].chars 
+  original_last_name = full[1].chars
 
-  original_last_name = full[1]
-
-new_first = []
-n= 0
-until n >= original_first_name.length
-  if original_first_name[n].match(/[aeiou]/)  
-      x = vowels.index(original_first_name[n])  + 1
-      z = vowels[x]
-      new_first.insert(n, z)
-  else   
-    k = consonants.index(original_first_name[n])  + 1
-    y = consonants[k]
-    new_first.insert(n, y)
+  new_first = []
+  n= 0
+  until n >= original_first_name.length
+    if original_first_name[n].match(/[aeiou]/)  
+        x = vowels.index(original_first_name[n])  + 1
+        z = vowels[x]
+        new_first.insert(n, z)
+    else   
+      k = consonants.index(original_first_name[n])  + 1
+      y = consonants[k]
+      new_first.insert(n, y)
+    end 
+    n += 1
   end 
-  n += 1
+  new_first= new_first.join("")
+  
+  new_last = []
+  n= 0
+  until n >= original_last_name.length
+    if original_last_name[n].match(/[aeiou]/)  
+        x = vowels.index(original_last_name[n])  + 1
+        z = vowels[x]
+        new_last.insert(n, z)
+    else   
+      k = consonants.index(original_last_name[n])  + 1
+      y = consonants[k]
+      new_last.insert(n, y)
+    end 
+    n += 1
+  end 
+  new_last= new_last.join("")
+  
+  new_full_name = new_last.capitalize + " " + new_first.capitalize
+
+  puts new_full_name
 end 
-new_first= new_first.join("")
-p new_first
 
- 
-
-  new_full_name = new_last + new_first #look up how to initialize array 
-
-end 
-
-
-
-=begin
-original_last_name.gsub!(/[o]/, 'o' => 'oo') 
-'hello'.gsub(/[aeiou]/, 'a' => 'e', 'e' => 'i', 'i' => 'o', 'o' => 'u', )    #=> "h3ll*"
-p original_last_name
-
-=end
+alias_maker()
