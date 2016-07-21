c1a4ecd161b2a7a1005970bfe11c5554f71c97cd
kimberlysuazo <chotoberli@hotmail.com>
2016-07-20 20:00:28 -0400

    Split name into array and then into two new strings

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -9,8 +9,21 @@ So 'a' would become 'e', 'u' would become 'a', and 'd' would become 'f'.
 =begin
 -  
 
+Remember .upcase at the end. right before creating new array. 
   
 =end
 
-full = "Kimberly Suazo"
-full.split("")
+#Puts "Please type in the full name to change into an alias."
+#full_name= gets.chomp
+
+#method(full_name)
+
+full_name = "Kimberly Suazo"
+full = full.split(" ")
+p full 
+
+original_first_name = full[0]
+p original_first_name
+original_last_name = full[1]
+p original_last_name
+
