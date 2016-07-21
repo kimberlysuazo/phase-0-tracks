f138bbf9de53b54f1da059aa671d9b46d81fe4fd
kimberlysuazo <chotoberli@hotmail.com>
2016-07-20 23:54:27 -0400

    Add user interface. Add loop.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,52 +1,55 @@
-
-def alias_maker 
-  puts "Please type in the full name to change into an alias."
+loop do 
+  puts "Please enter the full name (first and last) to change into an alias. When done type \"quit\"."
   full_name= gets.chomp.downcase
-  
-  vowels= "aeioua"
-  consonants = "bcdfghjklmnpqrstvwxyzb"
+  break if full_name == "quit" 
+   
+  def alias_maker (full_name)
+    
+    vowels= "aeioua"
+    consonants = "bcdfghjklmnpqrstvwxyzb"
 
-  full = full_name.split(" ")
-  original_first_name = full[0].chars 
-  original_last_name = full[1].chars
+    full = full_name.split(" ")
+    original_first_name = full[0].chars 
+    original_last_name = full[1].chars
 
-  new_first = []
-  n= 0
-  until n >= original_first_name.length
-    if original_first_name[n].match(/[aeiou]/)  
-        x = vowels.index(original_first_name[n])  + 1
-        z = vowels[x]
-        new_first.insert(n, z)
-    else   
-      k = consonants.index(original_first_name[n])  + 1
-      y = consonants[k]
-      new_first.insert(n, y)
+    new_first = []
+    n= 0
+    until n >= original_first_name.length
+      if original_first_name[n].match(/[aeiou]/)  
+          x = vowels.index(original_first_name[n])  + 1
+          z = vowels[x]
+          new_first.insert(n, z)
+      else   
+          k = consonants.index(original_first_name[n])  + 1
+          y = consonants[k]
+          new_first.insert(n, y)
+      end 
+      n += 1
     end 
-    n += 1
-  end 
-  new_first= new_first.join("")
-  
-  new_last = []
-  n= 0
-  until n >= original_last_name.length
-    if original_last_name[n].match(/[aeiou]/)  
-        x = vowels.index(original_last_name[n])  + 1
-        z = vowels[x]
-        new_last.insert(n, z)
-    else   
-      k = consonants.index(original_last_name[n])  + 1
-      y = consonants[k]
-      new_last.insert(n, y)
+    new_first= new_first.join("")
+    
+    new_last = []
+    n= 0
+    until n >= original_last_name.length
+      if original_last_name[n].match(/[aeiou]/)  
+          x = vowels.index(original_last_name[n])  + 1
+          z = vowels[x]
+          new_last.insert(n, z)
+      else   
+          k = consonants.index(original_last_name[n])  + 1
+          y = consonants[k]
+          new_last.insert(n, y)
+      end 
+      n += 1
     end 
-    n += 1
+    new_last= new_last.join("")
+    
+    new_full_name = new_last.capitalize + " " + new_first.capitalize
+
+
+    puts "Your new spy alias is: #{new_full_name}"
   end 
-  new_last= new_last.join("")
-  
-  new_full_name = new_last.capitalize + " " + new_first.capitalize
 
-
-  puts "Your new spy alias is: #{new_full_name}"
+  alias_maker(full_name)
 end 
-
-alias_maker()
 
