c4564e573aeda6a4835b0901d913c28e7f95c0ee
kimberlysuazo <chotoberli@hotmail.com>
2016-07-21 14:29:03 -0400

    Add comments and pseudocode to complete assignment.

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,5 +1,24 @@
 
 
+#RELEASE 0 
+=begin
+Search_array method takes an array of integers and an integer to look for. 
+  If the integer is found within the array 
+    - If returns its index within the array
+  If not
+    -It returns nil 
+=end
+
+def search_array2(array, int)
+  x = nil
+  array.each_index { |i| if array[i] == int then x = i end }
+  x
+end
+
+#p search_array2(array, int)
+
+#Below is the first version of the search array, the longer version, which we wrote out first. 
+=begin
 def search_array(array, int)
   x = nil
   array.each_index do |i|
@@ -9,23 +28,13 @@ def search_array(array, int)
   end
   x
 end
+=end 
 
-array = [8, 9, 10, 11, 12]
-int = 11
+#RELEASE 1
+=begin
+The fib method takes in a number and returns an array of Fibonacci numbers that is the length of the number entered. 
 
-# p search_array(array, int)  
-
-
-def search_array2(array, int)
-  x = nil
-  array.each_index { |i| if array[i] == int then x = i end }
-  x
-end
-
-# p search_array2(array, int)
-
-#-----------------------------------------------------------------------------------------
-
+=end 
 def fib(num)
   sequence = [0, 1]
   index_a = 0
@@ -42,20 +51,21 @@ def fib(num)
   sequence 
 end
 
-#p fib(100)
+#p fib(6)
 
-
-# PSEUDOCODE FOR SORT_SQUARES
+#RELEASE 2
 =begin
+Sort_squares method takes an array of integers. 
+It squares every other number and then returns that new array in ascending order. 
+PSEUDOCODE FOR SORT_SQUARES
  -Square every other item in array. 
-  -We would like the final array to be in ascending order. 
-  -Create a loop  
-   - If the item at current index is bigger than item at next index, 
-    - swap items
-  -If not, then you dont swap. 
-  -Continue looping through array until there are not more swaps. 
-  -Return new array. 
-
+  -We would like the final array to be in ascending order. Here we use the bubble_sort method which: 
+    -Creates a loop  
+     - If the item at current index is bigger than item at next index, 
+      - swap items
+    -If not, then you don't swap. 
+    -Continue looping through array until there are not more swaps. 
+    -Return new array. 
 =end
 
 def bubble_sort(array)
@@ -72,16 +82,14 @@ def bubble_sort(array)
 
     break if not swapped
   end
-
   array
 end
 
 def sort_squares(array)
-  
   array.each_index { |i| if i.even? then array[i]= array[i]**2 end }
   bubble_sort(array)
 end 
 
-array = [23, 4, 2, 45, 0]
-p fib(15)
-p sort_squares(fib(15))
+#array = [23, 4, 2, 45, 0]
+#p fib(15)
+#p sort_squares(fib(15))
