cf62392a89c14e829aea71eb988be5878eccca98
kimberlysuazo <chotoberli@hotmail.com>
2016-07-20 22:46:55 -0400

    Complete method to re-write first name

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -14,16 +14,48 @@ Remember .upcase at the end. right before creating new array.
 =end
 
 #Puts "Please type in the full name to change into an alias."
-#full_name= gets.chomp
+#full_name= gets.chomp.downcase
 
-#method(full_name)
+#alias_maker(full_name)
+def alias_maker
+  vowels= "aeiou"
+  consonants = "bcdfghjklmnpqrstvwxyz"
 
-full_name = "Kimberly Suazo"
-full = full.split(" ")
-p full 
+  full_name = "kimberly suazo"
+  full = full_name.split(" ")
+ 
+  original_first_name = full[0].chars 
 
-original_first_name = full[0]
-p original_first_name
-original_last_name = full[1]
+  original_last_name = full[1]
+
+new_first = []
+n= 0
+until n >= original_first_name.length
+  if original_first_name[n].match(/[aeiou]/)  
+      x = vowels.index(original_first_name[n])  + 1
+      z = vowels[x]
+      new_first.insert(n, z)
+  else   
+    k = consonants.index(original_first_name[n])  + 1
+    y = consonants[k]
+    new_first.insert(n, y)
+  end 
+  n += 1
+end 
+new_first= new_first.join("")
+p new_first
+
+ 
+
+  new_full_name = new_last + new_first #look up how to initialize array 
+
+end 
+
+
+
+=begin
+original_last_name.gsub!(/[o]/, 'o' => 'oo') 
+'hello'.gsub(/[aeiou]/, 'a' => 'e', 'e' => 'i', 'i' => 'o', 'o' => 'u', )    #=> "h3ll*"
 p original_last_name
 
+=end
