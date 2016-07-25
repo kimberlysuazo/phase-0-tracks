24f9405f443a8d8f4f3b2dac684e059e205796d3
kimberlysuazo <chotoberli@hotmail.com>
2016-07-25 16:21:47 -0400

    Add puppy class design (release 0)

---
+++ ruby/puppy.rb
@@ -0,0 +1,25 @@
+
+  
+#PUPPY CLASS DESIGN 
+=begin
+SPECIES ------------------------
+Canis Lupus Puppis 
+
+CHARACTERISTICS ----------------
+Legs: 4
+Fur: yes 
+Tail: yes
+Friendly: yes 
+Name: varies
+Color: varies
+
+BEHAVIOR -----------------------
+Bark
+Lick
+Jump
+Fetch 
+Run 
+Likes to bury bones
+Enjoys being scratched  
+=end
+
