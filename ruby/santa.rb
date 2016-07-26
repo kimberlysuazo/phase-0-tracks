d5764db9f9f38a1aeffae1aa6879a6e95d49d9fe
kimberlysuazo <chotoberli@hotmail.com>
2016-07-26 12:59:39 -0400

    Declare Santa class and add 3 instance methods

---
+++ ruby/santa.rb
@@ -0,0 +1,15 @@
+class Santa
+  def initialize
+    puts "Initializing Santa instance ..."
+  end 
+  
+  def speak
+    puts "Ho, ho, ho! Haaaappy holidays!"
+  end 
+
+  def eat_milk_and_cookies(cookie_type)
+    puts "That was a good #{cookie_type} cookie!" 
+    cookie
+  end   
+
+end   
