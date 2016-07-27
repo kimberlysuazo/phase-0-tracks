595f82675951ce80a57e2cdc230641b71b86cd0d
kimberlysuazo <chotoberli@hotmail.com>
2016-07-26 20:03:49 -0400

    Add yell_happily module method. Add driver code to test both methods

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -2,6 +2,13 @@ module Shout
   def self.yell_angrily(words)
     words + "!!!" + " :("
   end
+
+  def self.yell_happily(words)
+    words + "!!" + " :D"
+  end
 end
 
+#Driver code test module methods 
+puts "Guy finds his car has been scratched at the parking lot. This is his reaction: #{Shout.yell_angrily("NO")}"
+puts "Guy finds $20 at the parking lot. This is his reaction: #{Shout.yell_happily("woo hoo")}"
 
