942c69e77aa89b5e4df29364f649680cbb293405
kimberlysuazo <chotoberli@hotmail.com>
2016-07-27 16:18:03 -0400

    Complete release 3. Create driver code and update methods.

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -35,7 +35,7 @@ def list_print(shopping_list)
   end
 end
 def create_list(string)
-  item_aray =string.split(" ")
+  item_array = string.split(" ")
   shopping_list = {}
   item_array.each do |item|
     shopping_list[item] = 1
@@ -44,17 +44,51 @@ def create_list(string)
   shopping_list
 end
 
-def add_item(list_item)
-  shopping_list[list_item] = 1
+def add_item(list_item,user_list)
+  # user_list = {}
+  user_list[list_item] = 1
   
-  shopping_list
+  user_list
 end
 
-def remove_item(list_item)
-  shopping_list.delete(list_item)
-  shopping_list
+def remove_item(list_item,user_list)
+  user_list.delete(list_item)
+  user_list
 end
- def update_quantity(list_item,quantity)
-   shopping_list[list_item] =  quantity
-   shoping_list
+ def update_quantity(list_item,quantity,user_list)
+   user_list[list_item] =  quantity
+   user_list
  end
+
+# driver code
+list = "lemonade tomatoes onions ice-cream"
+user_list = create_list(list)
+puts user_list
+
+add_item('apple', user_list)
+p user_list 
+remove_item('apple', user_list)
+p user_list 
+update_quantity('lemonade', 2, user_list)
+p user_list
+list_print(user_list)
+
+#release 3
+update_quantity('tomatoes', 3, user_list)
+update_quantity('ice-cream', 4, user_list)
+remove_item('lemonade', user_list)
+list_print(user_list)
+update_quantity('ice-cream', 1, user_list)
+list_print(user_list)
+
+# add_item("apple")
+
+
+# create your list
+# add an item to your list
+# update an item quanitty
+
+
+
+#User Interface 
+#puts "Please enter items you would like to add to your shopping list followed by ENTER after each item. When finished type \"done\"."
