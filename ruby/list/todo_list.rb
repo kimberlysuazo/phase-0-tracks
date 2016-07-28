09d3f552edefaabf46b4ea4344b6f46b89943bb1
kimberlysuazo <chotoberli@hotmail.com>
2016-07-28 18:06:28 -0400

    Add delete_item and get_item methods

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -1,8 +1,22 @@
 class TodoList
   attr_reader :get_items
-  
+
   def initialize(list)
     @get_items = list 
   end   
 
-end   
+  def add_item(item)
+    @get_items << item
+  end  
+
+  def delete_item(item)
+    @get_items.delete(item)
+  end  
+
+  def get_item(index)
+    @get_items[index]
+
+  end   
+end  
+
+
