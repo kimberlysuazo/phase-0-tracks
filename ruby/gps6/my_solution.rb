1e2a00f8d28b3af742d4a09f41915a1b79fe6337
kimberlysuazo <chotoberli@hotmail.com>
2016-08-02 00:02:26 -0400

    Add reflection

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -7,7 +7,6 @@
 #require_relative is a way to load code into your file. It's a more convenient form of 'require'. 
   #It's used with the relative path of the class file that you're pulling into your file. 
 #require on the other hand uses an absolute path to the class file you're pulling.  
-
 require_relative 'state_data'
 
 class VirusPredictor
@@ -63,3 +62,31 @@ end
 
 #=======================================================================
 # Reflection Section
+=begin
+What are the differences between the two different hash syntaxes shown in the state_data file?
+  One declares the key in and uses a hash rocket => to declare its value like so, "key" => value. 
+  The other method declares the key using a symbol, but when declaring it, the colon goes after the symbol, 
+  like so, key: value. 
+
+What does require_relative do? How is it different from require?
+  require_relative is a way to load pre-existing code into your file. It's a more convenient form of 'require'. 
+  It's used with the relative path of the class file that you're pulling into your file. require on the other hand 
+  uses an absolute path to the class file you're pulling. 
+
+What are some ways to iterate through a hash?
+  You can iterate through a hash using #each, #each_pair, #each_key, #each_value, #select, #delete,  
+
+When refactoring virus_effects, what stood out to you about the variables, if anything?
+  What stood out was that the parameters were very repetitive and the variables were present in each of the methods 
+  and also outside of the methods. 
+
+What concept did you most solidify in this challenge?
+  This challenge was very helpful in thinking about instance variables declared in a class and their scope. 
+  Furthermore, it clarified the concept of require_relative vs require, and this challenge gave the me opportunity to understand 
+  the different types of variables other than local, instance, and global. I wasnt familiar with Constant variables.
+  Finally, my pair re-introduced me to case statements, particularly the possibility of declaring a new variable with the case statement,
+  which is very handy for refactoring. 
+=end 
+
+
+
