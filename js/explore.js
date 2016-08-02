a2f3cd036ab931be5382e6f5c46af08cc9119d18
kimberlysuazo <chotoberli@hotmail.com>
2016-08-02 17:25:44 -0400

    Add pseudocode

---
+++ js/explore.js
@@ -0,0 +1,9 @@
+// Pseudocode 
+// Reverse function: a function that takes in a strings and returns it reversed 
+// Input: A string. 
+//  Steps: Loop through each index of the string descending from the largest index.
+//         - Take each letter and add that to a new string. 
+//         - Print that new string to the console. 
+//  Output: New string.          
+
+
