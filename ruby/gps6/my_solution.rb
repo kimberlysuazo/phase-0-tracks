7c2735e4a5da52c26900f1097ef6463a0118a092
kimberlysuazo <chotoberli@hotmail.com>
2016-08-01 23:33:51 -0400

    Edit indenting and comments for class declaration and driver code

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -1,77 +1,14 @@
-#population density is number of people per square mile as of 2012
-#this data is updated every year with estimates from a 10 year census
-
-
-STATE_DATA = {
-  "Alabama" => {population_density: 94.65, population: 4822023},
-  "Alaska" => {population_density: 1.1111, population: 731449},
-  "Arizona" => {population_density: 57.05, population: 6553255},
-  "Arkansas" => {population_density: 56.43, population: 2949131},
-  "California" => {population_density: 244.2, population: 38041430},
-  "Colorado" => {population_density: 49.33, population: 5187582},
-  "Connecticut" => {population_density: 741.4, population: 3590347},
-  "Delaware" => {population_density: 470.7, population: 917092},
-  "Florida" => {population_density: 360.2, population: 19317568},
-  "Georgia" => {population_density: 172.5, population: 9919945},
-  "Hawaii" => {population_density: 216.8, population: 1392313},
-  "Idaho" => {population_density: 19.15, population: 1595728},
-  "Illinois" => {population_density: 231.9, population: 12875255},
-  "Indiana" => {population_density: 182.5, population: 6537334},
-  "Iowa" => {population_density: 54.81, population: 3074186},
-  "Kansas" => {population_density: 35.09, population: 2885905},
-  "Kentucky" => {population_density: 110.0, population: 4380415},
-  "Louisiana" => {population_density: 105.0, population: 4601893},
-  "Maine" => {population_density: 43.04, population: 1329192},
-  "Maryland" => {population_density: 606.2, population: 5884563},
-  "Massachusetts" => {population_density: 852.1, population: 6646144},
-  "Michigan" => {population_density: 174.8, population: 9883360},
-  "Minnesota" => {population_density: 67.14, population: 5379139},
-  "Mississippi" => {population_density: 63.50, population: 2984926},
-  "Missouri" => {population_density: 87.26, population: 6021988},
-  "Montana" => {population_density: 6.86, population: 1005141},
-  "Nebraska" => {population_density: 23.97, population: 1855525},
-  "Nevada" => {population_density: 24.8, population: 2758931},
-  "New Hampshire" => {population_density: 147.0, population: 1320718},
-  "New Jersey" => {population_density: 1205, population: 8864590},
-  "New Mexico" => {population_density: 17.16, population: 2085538},
-  "New York" => {population_density: 415.3, population: 19570261},
-  "North Carolina" => {population_density: 200.6, population: 9752073},
-  "North Dakota" => {population_density: 9.92, population: 699628},
-  "Ohio" => {population_density: 282.5, population: 11544225},
-  "Oklahoma" => {population_density: 55.22, population: 3814820},
-  "Oregon" => {population_density: 40.33, population: 3899353},
-  "Pennsylvania" => {population_density: 285.3, population: 12763536},
-  "Rhode Island" => {population_density: 1016, population: 1050292},
-  "South Carolina" => {population_density: 157.1, population: 4723723},
-  "South Dakota" => {population_density: 10.86, population: 833354},
-  "Tennessee" => {population_density: 156.6, population: 6456243},
-  "Texas" => {population_density: 98.07, population: 26059203},
-  "Utah" => {population_density: 34.3, population: 2855287},
-  "Vermont" => {population_density: 67.73, population: 626011},
-  "Virginia" => {population_density: 207.3, population: 8185867},
-  "Washington" => {population_density: 102.6, population: 6724540},
-  "Washington,D.C."=> {population_density: 10357, population: 632323},
-  "West Virginia" => {population_density: 77.06, population: 1855413},
-  "Wisconsin" => {population_density: 105.2, population: 5726398},
-  "Wyoming" => {population_density: 5.851, population: 576412}
-}
-
-
-
-
 # Virus Predictor
 
-# I worked on this challenge [by myself, with: ].
-# We spent [#] hours on this challenge.
+# I worked on this challenge [by myself, with: Keri Gannon].
+# We spent 2 hours on this challenge.
 
 # EXPLANATION OF require_relative
-#
-#
-#require_relative 'state_data'
-#require_relative is used for the relative path of the class file that you're pulling into your file. 
-#require on the other hand uses a absolute path to the class file you're pulling.  
+#require_relative is a way to load code into your file. It's a more convenient form of 'require'. 
+  #It's used with the relative path of the class file that you're pulling into your file. 
+#require on the other hand uses an absolute path to the class file you're pulling.  
 
-
+require_relative 'state_data'
 
 class VirusPredictor
 #The initialize method will give each instance of that class the corresponding state, population and population density attribute information.
@@ -81,46 +18,48 @@ class VirusPredictor
     @population_density = population_density
   end
 
-#The virus_effects method, calls the predicted_deaths and the speed_of_spread methods defined below.   
+#The virus_effects method calls the predicted_deaths and the speed_of_spread methods defined below and prints the statement.   
   def virus_effects
      print "#{@state} will lose #{predicted_deaths} people in this outbreak 
-     and will spread across the state in #{speed_of_spread} months. \n\n"
+     and the outbreak will spread across the state in #{speed_of_spread} months. \n\n"
   end
 
   private
 
-#The predicted_deaths method takes into account the population density of each state, and depending on its amount, calculates a specific number of deaths predicted for that state. The output is the printed statement saying the number of deaths in the state for this outbreak.  
-  def predicted_deaths(population_density, population, state)
+#The predicted_deaths method takes into account the population density of each state, and depending on its amount, calculates a specific number 
+#of deaths predicted for that state. The output is the predicted number of deaths in the state for this outbreak.  
+  def predicted_deaths
     # predicted deaths is solely based on population density
     y = case @population_density
-    when (150..199) then 0.3
-    when (100..149) then 0.2
-    when (50..99) then 0.1
-    when (0..49) then 0.05
-    else 0.4
-    end 
-      (@population * y).floor
-
-#The speed_of_spread method also takes into account the population density and depending on that figure, assigns a speed at which the outbreak will spread accross the state. Its output is the printed statement. 
-  
-  def speed_of_spread(population_density, state) #in months
-    # We are still perfecting our formula here. The speed is also affected
-    # by additional factors we haven't added into this functionality.
-    speed = 0.0
-
-  speed = case @population_density
-    when (150..199) then 1
-    when (100..149) then 1.5
-    when (50..99) then 2
-    when (0..49) then 2.5
-    else 0.5
-    end 
-    
-    
-
+      when (0..49) then 0.05
+      when (50..99) then 0.1
+      when (100..149) then 0.2  
+      when (150..199) then 0.3
+      else 0.4
+      end 
+    (@population * y).floor
+  end 
+#The speed_of_spread method also takes into account the population density and depending on that figure, assigns a speed at which the outbreak 
+#will spread across the state. Its output is the months it will take to spread across the state. 
+  def speed_of_spread #in months
+    speed = case @population_density
+      when (0..49) then 2.5
+      when (50..99) then 2 
+      when (100..149) then 1.5 
+      when (150..199) then 1
+      else 0.5
+      end
   end
-    
-  puts " and will spread across the state in #{speed} months.\n\n"
-  end
-
 end
+
+#=======================================================================
+
+# DRIVER CODE
+ # initialize VirusPredictor for each state
+STATE_DATA.each do |state, state_info|
+  new_instance = VirusPredictor.new(state, state_info[:population_density], state_info[:population])
+  new_instance.virus_effects
+end
+
+#=======================================================================
+# Reflection Section
