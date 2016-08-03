4ed0158015b10c909eb15eeb6ac8b7e4e01ecf32
kimberlysuazo <chotoberli@hotmail.com>
2016-08-03 16:57:22 -0400

    relese 1

---
+++ js/data_structures.js
@@ -0,0 +1,8 @@
+var colors = ["pink", "orange", "brown", "green"];
+var horses = ["Mr. Ed", "Lightning", "Secretariat", "George"];
+
+colors.push("red");
+horses.push("Bill");
+
+console.log (colors);
+console.log (horses);
