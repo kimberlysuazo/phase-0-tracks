46c22b2508b323d90d7a9b53ec2c6d52c3cf4fa5
kimberlysuazo <chotoberli@hotmail.com>
2016-08-03 16:57:53 -0400

    Add car constructor

--- js/data_structures.js
+++ js/data_structures.js
@@ -6,3 +6,29 @@ horses.push("Bill");
 
 console.log (colors);
 console.log (horses);
+
+var ourHorses = {};
+
+for (var i = 0; i < colors.length; i++)
+{
+  ourHorses[horses[i]] = colors[i];
+}
+
+console.log(ourHorses);
+
+
+function Car(year, make, color)
+    {
+      this.year = year;
+      this.make = make;
+      this.color = color;
+      
+      this.drive = function() 
+      {
+        console.log("Vroom, vroom!");
+      };
+    }
+      
+var saab = new Car(2013, "sedan", "black");
+console.log(saab);
+saab.drive();
