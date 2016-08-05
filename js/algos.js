4311ab22dfb26458bcc9cb2bc6b45fe206aa8570
kimberlysuazo <chotoberli@hotmail.com>
2016-08-05 14:29:49 -0400

    Edit longest_value function, edit pseudocode, and test in driver code

--- js/algos.js
+++ js/algos.js
@@ -2,29 +2,32 @@
 // longest_value function will take in an array of strings and return the string that is the longest. 
 // Input: An array 
     // Steps: 
+      // set an empty lengths array 
+      // declare new max variable with value of 0 
       //evaluate string at each index (loop)
         //Find length of each string 
         // push those lengths to store on the new lengths array. 
-      // compare indexes to select which one is larger. select one with longest length 
-      // return that index 
-      // using that index, match the appropriate phrase. 
+      // Set a loop that will go through each of the lengths array
+        // it will compare the length to max. 
+          // IF that length is larger, it will set that as the max variable value.
+      // Method will return the longest string by using the index of max in the lengths array 
+      // and then using that index to access the string in the original array      
 // Output: A string
-
 
 function longest_value(array) {
 var lengths = []; 
-var max = [0];
+var max = 0;
   array.forEach(function (item, index, array) {
     lengths.push(item.length);
   });
  for (var counter = 0; counter < lengths.length; counter ++){
-    if (max[0] < lengths[counter]) {
-      max.unshift(lengths[counter]);
+    if (max < lengths[counter]) {
+      max = (lengths[counter]);
     } 
   } 
-console.log(max[0]) 
+return array[lengths.indexOf(max)];
 } 
 
 // driver code 
 var phrases = ["long phrase","longest phrase","longer phrase"];
-longest_value(phrases);
+console.log(longest_value(phrases));
