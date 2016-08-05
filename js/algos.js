dd329feaad6006f8b644382a438b96ab762a69b1
kimberlysuazo <chotoberli@hotmail.com>
2016-08-05 16:34:09 -0400

    Pseudocode second function, start coding it, test that it separates keys and values into arrays with driver code

--- js/algos.js
+++ js/algos.js
@@ -1,4 +1,5 @@
 // Pseudocode 
+
 // longestItem function will take in an array of strings and return the string that is the longest. 
 // Input: An array 
     // Steps: 
@@ -31,19 +32,54 @@ return array[lengths.indexOf(max)];
 // objectComparer function will take in two objects, and compare their key-value pairs to give. 
 // If any keys or values match, function will return true. if not, function will return false. 
   // Input: Two objects 
-  // Steps:
-    // 
+  // Steps: 
+    // Make arrays of the values and keys of each object. 
+    // To check for matching keys, 
+      // Loop through the keys in object 1, to see if any of them are keys in object 2. 
+        
+             // if (allKeys.indexOf(key)) {var keyIndex = (allKeys.indexOf(key))}
+          // IF there is a match 
+              // Store it's index.  
+              // Check for matching values,
+                  // Loop through the values of object 1, to see if any of them are values in object 2. 
+                    // IF there is a match, store its index. 
+                     // if (allValues.indexOf(value)) { var keyValue = console.log(allValues.indexOf(value))}
+                        // IF keyIndex equals, valueIndex, then function returns TRUE. 
+                        // ELSE function returns FALSE 
+          //ELSE function returns FALSE
+                     
+function objectComparer(object1, object2) {
+// get all keys into arrays 
+var allKeys1 = Object.keys(object1);
+var allKeys2 = Object.keys(object2);
+var allValues1 = [];
+var allValues2 = [];
+  // get all property values into arrays 
+  for(var key in object1) {
+      allValues1.push(object1[key]);
+  }
+  for(var key in object2) {
+      allValues2.push(object2[key]);
+  }
+console.log(allKeys1);
+console.log(allKeys2);
+console.log(allValues1);
+console.log(allValues2);
+}
 
-// In algos.js, write a function that takes two objects and checks to 
-// see if the objects share at least one key-value pair. 
-// If we called your function with {name: "Steven", age: 54} and {name: "Tamir", age: 54}, the function would return true, 
-// because at least one key-value pair matches between the two objects. If no pairs match 
-// (and keep in mind that the two objects may not even have any of the same keys), 
-// the function should return false. To make your life easier, don't worry about whether a property is a string 
-// ('age') or an identifier name (age). Those can be considered equivalent. 
 
 // Add some driver code that tests both outcomes of your function.
 
 // driver code 
 var phrases = ["long phrase","longest phrase","longer phrase"];
 console.log(longestItem(phrases));
+
+// create objects to test objectCompare
+var dog = {name: 'Spot', 'age': 3};
+var humans = {name: 'John', 'age': 3};
+var cars = {make: 'saab', year: 2015, color: 'black'};
+
+objectComparer(dog, cars);
+
+
+
