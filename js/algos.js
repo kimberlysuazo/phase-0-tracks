47723af1371fbbb3e04f61988bc9588d79371523
kimberlysuazo <chotoberli@hotmail.com>
2016-08-06 00:05:14 -0400

    Edit comments and pseudocode

--- js/algos.js
+++ js/algos.js
@@ -1,5 +1,4 @@
 // Pseudocode 
-
 // longestItem function will take in an array of strings and return the string that is the longest. 
 // Input: An array 
     // Steps: 
@@ -33,20 +32,13 @@ return array[lengths.indexOf(max)];
 // If any keys or values match, function will return true. if not, function will return false. 
   // Input: Two objects 
   // Steps: 
-    // Make arrays of the values of each object. 
-    // To check for matching keys, 
-      // Loop through object 1, to see if any of the keys are keys in object 2. 
-        // Loop through the values of object 1, to see if any of them are values in object 2. 
-            // IF both conditions in the loop are true. 
+    // To check for matching key-value pair, 
+      // Loop through object 1, 
+          // Check if the key is the value is the same both in object 1 and object 2, using the same key.
+          // Only returns true if there is a key-value match.  
+              // IF returns true 
                  // function returns TRUE.              
-        //ELSE function returns FALSE
-
-// driver code 
-// var phrases = ["long phrase","longest phrase","longer phrase"];
-// console.log(longestItem(phrases));
-
-// // create objects to test objectCompare
-
+      //ELSE function returns FALSE.
                      
 function objectComparer(object1, object2) {
   for(var key1 in object1) {
@@ -58,10 +50,20 @@ function objectComparer(object1, object2) {
   return false; 
  }
  
-var dog = {name: 'Spot', 'age': 3};
-var humans = {name: 'John', 'age': 3};
-var cars = {make: 'saab', year: 3, color: 'black', name: 3 };
+// driver code 
+// create array to test longestItem
+var phrases = ["long phrase","longest phrase","longer phrase"];
+console.log(longestItem(phrases));
 
+// create objects to test objectCompare
+var dog = {'age': 3, name: 'Spot'};
+var humans = {name: 'John', age: 3};
+var cars = {make: 'Ford', year: 3, color: 'black', name: 'Fiesta'};
+var pen = {tip: 'ballpoint', 'ink': 'red'}
 
+// compare two objects with same keys (in different order), and one matching key-value pair 
 console.log(objectComparer(dog, humans));
+// compare two objects that share one key, one value, but not a key-value pair match
 console.log(objectComparer(dog, cars));
+// compare two objects that have no similar keys
+console.log(objectComparer(pen, cars));
