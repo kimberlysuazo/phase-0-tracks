8292e042bcba4c15efdd9057b9648bccce10c27f
kimberlysuazo <chotoberli@hotmail.com>
2016-08-12 22:16:03 -0400

    Add third table and populate it with fake data. Edit members table.

--- databases/fit_tracker/fit_tracker.rb
+++ databases/fit_tracker/fit_tracker.rb
@@ -49,6 +49,7 @@ OUTLINE for user interface:
 require 'sqlite3'
 require 'faker'
 
+
 # create SQLite3 database
 db = SQLite3::Database.new("fit_info.db")
 db.results_as_hash = true
@@ -57,16 +58,27 @@ create_table_cmd = <<-SQL
   CREATE TABLE IF NOT EXISTS members(
     id INTEGER PRIMARY KEY,
     name VARCHAR(255),
-    starting_weight INT, 
-    current_weight INT
+    starting_weight INT
   )
 SQL
 
 create_table2 = <<-SQL
   CREATE TABLE IF NOT EXISTS calories(
     id INTEGER PRIMARY KEY,
-    date TEXT,
+    day TEXT,
+    month TEXT,
     amt_burned INT, 
+    member_id INT,
+    FOREIGN KEY (member_id) REFERENCES members(id)
+  )
+SQL
+
+create_table3 = <<-SQL
+  CREATE TABLE IF NOT EXISTS weight(
+    id INTEGER PRIMARY KEY,
+    day TEXT,
+    month TEXT,
+    current_weight INT,
     member_id INT,
     FOREIGN KEY (member_id) REFERENCES members(id)
   )
@@ -79,26 +91,56 @@ db.execute(create_table_cmd)
 # create a calories table (if it's not there already)
 db.execute(create_table2)
 
+#create a weight table to keep track of weight over time 
+db.execute(create_table3)
 
-#today = Time.now.strftime("%m-%d")
-
-#create members method
+# method that creates members
 def create_member(db, name, starting_weight)  
-  #starting_weight = rand(115..300)
-  current_weight = starting_weight
-  db.execute("INSERT INTO members (name, starting_weight, current_weight) VALUES (?, ?, ?)", [name, starting_weight, current_weight])
+  db.execute("INSERT INTO members (name, starting_weight) VALUES (?, ?)", [name, starting_weight])
 end
+
+# method that creates calorie entries for members 
+def enter_calories(db, day, amt_burned, member_id)  
+  month = Time.now.strftime("%m") 
+  db.execute("INSERT INTO calories (day, month, amt_burned, member_id) VALUES (?, ?, ?, ?)", [day, month, amt_burned, member_id]) 
+end
+
+# method that creates entries for weight 
+def enter_weight(db, day, current_weight, member_id)
+  month = Time.now.strftime("%m") 
+  db.execute("INSERT INTO weight (day, month, current_weight, member_id) VALUES (?, ?, ?, ?)", [day, month, current_weight, member_id])
+end   
 
 # #create test member
-# create_member(db, Faker::Name.name, rand(115..300))
-member_print = db.execute("SELECT * FROM members")
-puts member_print
-
-# create calorie entries for members 
-def enter_calories(db, date, amt_burned, member_id)  
-  db.execute("INSERT INTO calories (date, amt_burned, member_id) VALUES (?, ?, ?)", [date, amt_burned, member_id]) 
-end
+# 6.times {create_member(db, Faker::Name.name, rand(115..300))}
+# member_print = db.execute("SELECT * FROM members")
+# puts member_print
 
 # create fake calories entry
-# enter_calories(db, '08-12', rand(200..1000), rand(1..5))
-# enter_calories(db, '08-08', rand(200..1000), rand(1..5))
+# enter_calories(db, '08-12', rand(200..1000), rand(1..6))
+# enter_calories(db, '08-08', rand(200..1000), rand(1..6))
+# enter_calories(db, '08-10', rand(200..1000), rand(1..6))
+# enter_calories(db, '08-06', rand(200..1000), rand(1..6))
+# enter_calories(db, '08-01', rand(200..1000), rand(1..6))
+# enter_calories(db, '08-02', rand(200..1000), rand(1..6))
+# enter_calories(db, '08-03', rand(200..1000), rand(1..6))
+# enter_calories(db, '08-09', rand(200..1000), rand(1..6))
+
+today = Time.now.strftime("%m-%d") #date in mm-dd format
+test_name= "Emily Kris"
+
+#calculate member_id
+find_id= db.execute("SELECT id FROM members WHERE name='#{test_name}'")
+person_id = find_id[0]['id']
+
+#update current weight for test member
+#enter_weight(db, today, 238, person_id)
+
+        ## weight_update= gets.chomp.to_i
+
+
+
+
+
+
+
