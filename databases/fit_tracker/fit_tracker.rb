18544db7132e18c6c5878f735dae07c10fc370a3
kimberlysuazo <chotoberli@hotmail.com>
2016-08-14 05:05:23 -0400

    complete UI, missing error handling for incorrect name

--- databases/fit_tracker/fit_tracker.rb
+++ databases/fit_tracker/fit_tracker.rb
@@ -58,7 +58,8 @@ create_table_cmd = <<-SQL
   CREATE TABLE IF NOT EXISTS members(
     id INTEGER PRIMARY KEY,
     name VARCHAR(255),
-    starting_weight INT
+    starting_weight INT, 
+    current_weight INT
   )
 SQL
 
@@ -97,7 +98,8 @@ db.execute(create_table3)
 
 # method that creates members
 def create_member(db, name, starting_weight)  
-  db.execute("INSERT INTO members (name, starting_weight) VALUES (?, ?)", [name, starting_weight])
+  current_weight = starting_weight
+  db.execute("INSERT INTO members (name, starting_weight, current_weight) VALUES (?, ?, ?)", [name, starting_weight, current_weight])
 end
 
 # method that creates calorie entries for members 
@@ -137,16 +139,18 @@ def weight_loss(db, member_id, time_frame)
       total_pounds << pounds['current_weight']
     end 
   end   
-  weight = db.execute("SELECT * FROM members WHERE id='#{member_id}'")
   weight_loss= total_pounds.max - total_pounds.min 
 end   
 
 # calculate total weight loss thus far 
 def total_weight_loss(db, member_id)  
+  total_pounds= []
   today = Time.now.strftime("%m-%d")  
-  weight = db.execute("SELECT * FROM members WHERE id='#{member_id}'")
-  updated_weight = db.execute("SELECT * FROM weight WHERE member_id='#{member_id}' AND day='#{today}' ")
-  weight_loss = weight[0]['starting_weight'] - updated_weight[0]['current_weight']
+  updated_weight = db.execute("SELECT * FROM weight WHERE member_id='#{member_id}'")
+  updated_weight.each do |pounds|
+      total_pounds << pounds['current_weight']
+    end 
+  weight_loss = total_pounds.max - total_pounds.min 
 end 
 
 #calculate calories month  
@@ -216,8 +220,8 @@ def weight_ranking(db, time_frame)
   new_ranking.each {|name, pounds| puts "#{name}: #{pounds} lbs lost"}
 end 
 
-# #create fake members
-# 6.times {create_member(db, Faker::Name.name, rand(115..300))}
+# # #create fake members
+#6.times {create_member(db, Faker::Name.name, rand(115..300))}
 
 # create fake calories entry
 # enter_calories(db, '08-12', rand(200..1000), rand(1..6))
@@ -247,35 +251,81 @@ members = db.execute("SELECT * FROM members")
 members.each {|user| puts user['name']}
 
 puts "\nAre you already part of the group? Please answer yes or no."
-answer= gets.chomp.downcase
-if answer == "yes"
-  puts "Please enter your name just as it appears on the list."
-  user_name= gets.chomp
-else 
-  puts "please enter your full name (First and Last) so you can join the wolf pack and track your way to healthy."  
-  user_name= gets.chomp
-  puts "Please enter your weight."
-  weight = gets.chomp.to_i
-  create_member(db, user_name, weight)
-end 
+while true 
+  answer= gets.chomp.downcase
+  if answer == "yes"
+    puts "Please enter your name EXACTLY as it appears on the list."
+    user_name= gets.chomp
+    break 
+  elsif answer == "no"
+    puts "please enter your full name (First and Last) so you can join the wolf pack and track your way to healthy."  
+    user_name= gets.chomp
+    puts "Please enter your weight."
+    weight = gets.chomp.to_i
+    create_member(db, user_name, weight)
+    created_member = true 
+    break
+  else 
+    puts "Please answer yes or no."  
+  end 
+end  
 
-day = Time.now.strftime("%m-%d") #date in mm-dd format
+
+
 member_id =find_id(db, user_name) #find the database member_id for current user. 
 
-puts "Hi #{user_name}. Please enter amount of calories you've burned today." 
+
+ 
+  
+day = Time.now.strftime("%m-%d") #date in mm-dd format
+
+puts "Hi #{user_name}."
+puts "Please enter the amount of calories you've burned today." 
 amt_burned = gets.chomp.to_i
 enter_calories(db, day, amt_burned, member_id)  
 
-puts "Please enter your current weight."
-current_weight= gets.chomp.to_i 
-enter_weight(db, day, current_weight, member_id)
+if !created_member
+  puts "Please enter your current weight."
+  current_weight= gets.chomp.to_i 
+  enter_weight(db, day, current_weight, member_id)
+end   
+
 
 puts "Here are your results, #{user_name}." 
 puts "In the last week, you've lost #{weight_loss(db, member_id, 'week')} lbs and burned #{calories_week(db, member_id)} calories."
 puts "This month, you've lost #{weight_loss(db, member_id, 'month')} lbs and burned #{calories_month(db, member_id)} calories."
 puts "Thus far you've lost #{total_weight_loss(db, member_id)} lbs, and have burned #{calories_burned(db, member_id)} calories."
 
+puts "\nWould you like to see the current group rankings? Please answer yes or no."
+see_ranking = gets.chomp.downcase
 
+if see_ranking == "yes"
+  puts "Here are the results for the Wolves Pack:" 
+  puts "\n_______Group rankings for this week_______"
+  puts "CALORIES================================"
+  calories_ranking(db, "week")
+  puts "WEIGHT LOSS ============================"
+  weight_ranking(db, "week")
+  puts "\n_______Group rankings for this month_______"
+  puts "CALORIES================================"
+  calories_ranking(db, "month")
+  puts "WEIGHT LOSS ============================"
+  weight_ranking(db, "month")
+  puts "\n_______Group rankings entire competiton________"
+  puts "CALORIES================================"
+  calories_ranking(db, "total")
+  puts "WEIGHT LOSS ============================"
+  weight_ranking(db, "total")
+end 
+
+
+
+
+
+
+# puts "weigh loss ranking:============================"
+# weight_ranking(db, "month")
+# weight_ranking(db, "total")
 =begin
 
 
@@ -307,10 +357,6 @@ puts "Thus far you've lost #{total_weight_loss(db, member_id)} lbs, and have bur
 
 
 
-# calories_ranking(db, "week")
-# puts "weigh loss ranking:============================"
-# weight_ranking(db, "month")
-# weight_ranking(db, "total")
 
 
 
