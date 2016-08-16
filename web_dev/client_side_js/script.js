83d223d00420cda41653b9e5df859188a36d82c3
kimberlysuazo <chotoberli@hotmail.com>
2016-08-16 15:44:12 -0400

    Add clickMe function and added event listerner for clickMe

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,27 +1,36 @@
 
-
+// This function adds new paragraph "Woo hoo!"
 function wooHoo(event){
- var newParagraph = document.createElement('p');
+  var newParagraph = document.createElement('p');
   var paraText= document.createTextNode('Woo Hoo!');
   newParagraph.appendChild(paraText);
- var body = document.getElementsByTagName('body');
+  var body = document.getElementsByTagName('body');
   body[0].appendChild(newParagraph);
- newParagraph.style.position= "relative";
- newParagraph.style.marginTop= "-270px";
- newParagraph.style.marginLeft= "500px";
+  newParagraph.style.position= "absolute";
+  newParagraph.style.marginTop= "-270px";
+  newParagraph.style.marginLeft= "500px";
 }
 
-// var newElement = document.getElementsByTagName('h1');
+// this function changes the hat's position
+function liftHat (event) {
+  event.target.style.marginTop = "-50px"
+}
+// this function changes the paragraph text
+function clickMe(event){
+  var parent = document.getElementsByTagName('body');
+  var child = document.getElementById('clickable');
+  var para = document.createElement('p');
+  var node = document.createTextNode("Now click on my glasses!");
+  para.appendChild(node);
+  parent[0].replaceChild(para, child);
+  para.style.fontSize = "40px";
+}
 
-// newElement.onclick = function() {
-//  console.log('clicked');
-// };
+// Add woo hoo AFTER glasses are clicked
+var glasses= document.getElementsByClassName("glasses"); 
+glasses[0].addEventListener("click", wooHoo);
 
-
-var ocho= document.getElementsByClassName("octopus"); 
-var ochoPic= ocho[0]
-ochoPic.addEventListener("click", wooHoo);
-
-
-.ondrag=function(){myScript};
-
+// Lifts hat and changes paragraph
+var hat= document.getElementsByClassName("hat"); 
+hat[0].addEventListener("click", liftHat);
+hat[0].addEventListener("click", clickMe);
