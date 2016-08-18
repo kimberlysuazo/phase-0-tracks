c5ed65744ee503404bf90cba8302929d81045f31
kimberlysuazo <chotoberli@hotmail.com>
2016-08-18 15:31:16 -0400

    add comments and organize methods in release order

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -12,38 +12,16 @@ get '/' do
   "#{params[:name]} is #{params[:age]} years old."
 end
 
-get '/great_job' do
-  name = params[:name]
-  if name 
-    "Good job, #{name}!"
-  else 
-    "Good job!"
-  end     
-
-end   
-
 # write a GET route with
 # route parameters
 get '/about/:person' do
   person = params[:person]
   "#{person} is a programmer, and #{person} is learning Sinatra."
-end
-
-get '/contact/:address' do 
-  address = params[:address]
-  "Our address is: #{address}"
-end   
+end  
 
 get '/:person_1/loves/:person_2' do
   "#{params[:person_1]} loves #{params[:person_2]}"
 end
-
-get '/:num_1/math/:num_2' do
-  num_1 = params[:num_1]
-  num_2 = params[:num_2]
-  math= num_2.to_i + num_1.to_i
-  "#{params[:num_1]} + #{params[:num_2]}= #{math}"
-end   
 
 # write a GET route that retrieves
 # all student data
@@ -66,3 +44,31 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+# ==================================================================================================
+# RELEASE 0
+
+get '/great_job' do
+  name = params[:name]
+  if name 
+    "Good job, #{name}!"
+  else 
+    "Good job!"
+  end     
+end 
+
+#  Route that displays address 
+get '/contact/:address' do 
+  address = params[:address]
+  "Our address is: #{address}"
+end
+
+# Route that adds two numbers 
+get '/:num_1/math/:num_2' do
+  num_1 = params[:num_1]
+  num_2 = params[:num_2]
+  math= num_2.to_i + num_1.to_i
+  "#{params[:num_1]} + #{params[:num_2]}= #{math}"
+end   
+
+
